20ac7fcf4c55ea1f2a672d6f8b2108ffb07d2ac4
Emmy Armintrout <emmy.armintrout@gmail.com>
2020-01-23 11:02:22 -0800

    try that

--- src/jobs.js
+++ src/jobs.js
@@ -5,6 +5,8 @@ import { sendSlack } from '/src/slack-webhook';
 
 const queue = new Bull(`slackbotQueue`, redisUrl);
 
+queue.empty();
+
 queue.process('slackbotQueue', async (job) => {
   await sendSlack();
 })
@@ -13,14 +15,12 @@ queue.on('completed', (job, result) => {
   console.log('success');
 });
 
-
 export const initJobs = async () => {
-  // await queue.empty();
   await queue.add('slackbotQueue', {}, {
     repeat: {
       // At 09:00 on Monday in January, February, March, April, May, September, October, November, and December
       // 5pm UTC = 9am PST
-      cron: '0 17,18,19,20,21 * * *'
+      cron: '* 17,18,19,20,21 * * *'
       // cron: '0 17 * 1,2,3,4,5,9,10,11,12 MON',
     }
   })
