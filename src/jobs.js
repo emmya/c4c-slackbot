d8bbc018bec6f7c2f1f199d614bfe8f1ea18ee6a
Emmy Armintrout <emmy.armintrout@gmail.com>
2020-01-30 10:41:00 -0800

    ugh

--- src/jobs.js
+++ src/jobs.js
@@ -31,7 +31,9 @@ export const initJobs = async () => {
     repeat: {
       // At 09:00 PT in January, February, March, April, May, September, October, November, and December
       // 5pm UTC = 9am PST
-      cron: '0 17 * 1,2,3,4,5,9,10,11,12 THU',
+      cron: '* * * 1,2,3,4,5,9,10,11,12 THU',
     }
   });
+
+  console.log('added jobs to queue.');
 }
